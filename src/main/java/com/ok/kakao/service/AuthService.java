7b4e2bb775e63ca17d57ec669fa2d6dce7cb61f2
OK <rlaehddhr26@naver.com>
2024-05-22 00:17:55 +0900

    [fix] log라이브러리 수정

--- src/main/java/com/ok/kakao/service/AuthService.java
+++ src/main/java/com/ok/kakao/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.ok.kakao.service;
 
 import org.json.JSONObject;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,11 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.logging.Logger;
 
 @Service
 public class AuthService extends HttpCallService{
-//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private static final String AUTH_URL = "https://kauth.kakao.com/oauth/token";
 
@@ -41,7 +41,7 @@ public class AuthService extends HttpCallService{
         accessToken = jsonData.get("access_token").toString();
         refrashToken = jsonData.get("refresh_token").toString();
         if(accessToken.isEmpty() || refrashToken.isEmpty()) {
-//            logger.debug("토큰발급에 실패했습니다.");
+            logger.debug("토큰발급에 실패했습니다.");
             return false;
         }else {
             authToken = accessToken;
