69ee29150bae01fc0f4c6803472262383944e0ea
OK <rlaehddhr26@naver.com>
2024-06-06 17:27:44 +0900

    [feat] 스케줄러 메세지발신 추가

--- src/main/java/com/ok/kakao/scheduler/MessageScheduler.java
+++ src/main/java/com/ok/kakao/scheduler/MessageScheduler.java
@@ -1,9 +1,14 @@
 package com.ok.kakao.scheduler;
 
+import com.ok.kakao.dto.DefaultMessageDto;
+import com.ok.kakao.service.AuthService;
+import com.ok.kakao.service.CustomMessageService;
 import com.ok.kakao.service.HttpCallService;
+import com.ok.kakao.service.MessageService;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,13 +17,23 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import java.util.Locale;
 
 @Component
 public class MessageScheduler extends HttpCallService {
 
-    @Scheduled(cron="0 44 18 * * ?")
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    CustomMessageService customMessageService;
+
+    @Scheduled(cron="0 43 16 * * ?")
     public void kakaoMessageScheduler() {
 
         String url = "https://kauth.kakao.com/oauth/authorize";
@@ -36,8 +51,21 @@ public class MessageScheduler extends HttpCallService {
         HttpEntity<?> requestEntity = httpClientEntity(header, parameters);
 
         ResponseEntity<String> response = httpRequest(url, HttpMethod.GET, requestEntity);
-        JSONObject jsonData = new JSONObject(response.getBody());
-        System.out.println(jsonData);
+    }
 
+    @Scheduled(cron="0 44 16 * * ?")
+    public void kakaoTestSendMessage() {
+        String authToken = authService.authToken;
+        if(!StringUtils.isEmpty(authToken)) {
+            DefaultMessageDto msgDto = new DefaultMessageDto();
+
+            msgDto.setBtnTitle("자세히보기");
+            msgDto.setMobileUrl("");
+            msgDto.setObjType("text");
+            msgDto.setWebUrl("");
+            msgDto.setText("스케줄러 테스트");
+
+            messageService.sendMessage(authToken, msgDto);
+        }
     }
 }
